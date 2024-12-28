3bec00c45b1204e25d870f316f411e8314288219
my-name <dipeshthada30@gmai.com>
2024-12-28 23:08:02 +0200

    making playable piano app

---
+++ playable-piano/piano.js
@@ -0,0 +1,20 @@
+const pianoKeys = document.querySelectorAll(".piano-keys")
+
+let audio = new Audio("../playable-piano/tune/a.wav") //by defult, adio src is "a tune"
+
+const playTune = (key) => {
+    audio.src = `../playable-piano/tune/${key}.wav`; //passing audio src based on key pressed
+    audio.play() //playing audio
+}
+
+pianoKeys.forEach(key => {
+    // calling playtune function with passing data-key value as an argument
+    key.addEventListener("click", () => playTune(key.dataset.key))
+
+})
+
+const pressedKey = (e) => {
+    console.log(e)
+}
+
+document.addEventListener("keydown", pressedKey)
