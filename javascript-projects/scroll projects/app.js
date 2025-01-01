8d2e8f1f605712b635c026294b54bd5f99327bde
my-name <dipeshthada30@gmai.com>
2025-01-01 20:59:11 +0200

    i made language translator

---
+++ javascript-projects/scroll projects/app.js
@@ -0,0 +1,75 @@
+// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
+// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
+// slice extracts a section of a string without modifying original string
+//offsetTop - A Number, representing the top position of the element, in pixels
+
+// ********** set date ************
+const date = document.getElementById("date")
+date.innerHTML = new Date().getFullYear()
+
+// ********** close links ************
+const navToggle = document.querySelector('.nav-toggle')
+const linkContainer = document.querySelector('.links-container')
+const links = document.querySelector('.links')
+
+navToggle.addEventListener('click', () => {
+    /* linkContainer.classList.toggle('show-links')
+    console.log(navToggle) */
+    const containerHeight = linkContainer.getBoundingClientRect().height
+    const linksHeight = links.getBoundingClientRect().height
+
+    if(containerHeight === 0) {
+        linkContainer.style.height = `${linksHeight}px`
+    }else {
+        linkContainer.style.height = 0
+    }
+    
+})
+const navbar = document.getElementById('nav')
+const topLink = document.querySelector('.top-link')
+
+// ********** fixed navbar ************
+window.addEventListener('scroll', () => {
+    const scrollHight = window.pageYOffset
+    const navHight = navbar.getBoundingClientRect().height
+    if(scrollHight > navHight) {
+        navbar.classList.add('fixed-nav')
+    }else{
+        navbar.classList.remove('fixed-nav')
+
+    } if(scrollHight > 500) {
+        topLink.classList.add('show-link')
+    }else{
+        topLink.classList.remove('show-link')
+    }
+})
+// ********** smooth scroll ************
+
+// select links
+const scrollLink = document.querySelectorAll('scroll-link')
+scrollLink.forEach(function(link) {
+    link.addEventListener('click', function(e) {
+        // prevent default
+        e.preventDefault()
+        // navigate to specific spot 
+        const id = e.currentTarget.getAttribute("href").slice(1)
+        const element = document.getElementById(id)
+        // calculate the height
+        const navHight = navbar.getBoundingClientRect().height
+        const containerHeight = linkContainer.getBoundingClientRect().height
+        const fixedNav = navbar.classList.contains("fixed-nav")
+        let position = element.offsetTop - navHight
+        if(!fixedNav){
+            position = position - navHight
+        }
+        if(navHight > 82) {
+            position = position + containerHeight
+        }
+
+        window.scrollTo({
+            left: 0,
+            top: position,
+        })
+        linkContainer.style.height = 0
+    })
+})
