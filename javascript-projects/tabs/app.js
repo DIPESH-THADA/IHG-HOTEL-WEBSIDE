73f670ebc109545e35f967ac19d00e5bc7d2baaa
my-name <dipeshthada30@gmail.com>
2025-01-03 20:56:21 +0200

    build a grocery bud and setup alla the javascript

--- javascript-projects/tabs/app.js
+++ javascript-projects/tabs/app.js
@@ -5,6 +5,16 @@ const articles = document.querySelectorAll('.content')
 about.addEventListener('click', (e) => {
     const id = e.target.dataset.id
     if(id) {
-        
+        // remove active from other buttons
+        btns.forEach((btn) => {
+            btn.classList.remove('active')
+            e.target.classList.add('active')
+        })
+        // hide all the article
+        articles.forEach((articles) => {
+            articles.classList.remove('active')
+        })
+        const element = document.getElementById(id)
+        element.classList.add('active')
     }
 })
